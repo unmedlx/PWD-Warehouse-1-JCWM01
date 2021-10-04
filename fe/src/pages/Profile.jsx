b6eb515297d9bbc7c82b86174cab97c683d78483
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2021-10-04 23:09:20 +0700

    [PW1J-41] Tampilan /Profile Gradient dan form tampilan data langsung edit
    Menambahkan warna gradient pada background div photo profile dan mengubah form data (menampilkan dan mengedit di satu field input)

--- fe/src/pages/Profile.jsx
+++ fe/src/pages/Profile.jsx
@@ -21,10 +21,11 @@ const Profile = () => {
                         <div className="">
                             <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Trulli" className="profilePicture" />
                         </div>
-                        <button className="btn btn-warning mt-3" onClick={handleShow}>Change Photo Profile</button>
+                        <button className="btn btn-dark mt-3" onClick={handleShow}>Change Photo Profile</button>
                         <div className="profile-photo-desc mt-3">
                             <p>Besar file: maksimum 10.000.000 bytes (10 Megabytes). Ekstensi file yang diperbolehkan: .JPG .JPEG .PNG</p>
                         </div>
+                        <button className="btn btn-light mt-3">Change Password</button>
                     </div>
 
                     <div className="profile-main">
@@ -36,14 +37,33 @@ const Profile = () => {
                         </div>
 
                         <div className="profile-main-detail">
-                            <h6><strong>Ubah Biodata Diri</strong></h6>
-                            <p>Name</p>
-                            <p>Tanggal Lahir</p>
-                            <p>Jenis Kelamin</p>
+                            <h1><strong>Hello, Nadhif Rafifaiz</strong></h1>
+                            <h6><strong>nadhifrafifaiz@gmail.com</strong></h6>
+                            <hr />
+                            <p><strong>Biodata Diri</strong></p>
+                            <div className="profile-main-data">
+                                <div className="input-container">
+                                    <input type="text" className="input-field" placeholder="Username" value={"nadhif_rafifaiz"} disabled />
+                                </div>
+                                <div className="input-container">
+                                    <input type="text" className="input-field" placeholder="Date of Birth" />
+                                </div>
+                                <div className="input-container">
+                                    <input type="text" className="input-field" placeholder="Email" />
+                                </div>
+                                <div className="input-container">
+                                    <input type="text" className="input-field" placeholder="Gender" />
+                                </div>
+                                <div className="input-container">
+                                    <input type="text" className="input-field" placeholder="Fullname" />
+                                </div>
+                            </div>
 
-                            <h6><strong>Ubah Kontak</strong></h6>
-                            <p>Name</p>
-                            <p>Email</p>
+                            <button className="btn btn-warning">Edit Data</button>
+
+
+
+
                         </div>
                     </div>
 
