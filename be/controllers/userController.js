b2686d38cab4b2364ab0d0d099c1795edb165188
unmedlx <luthfiayyas@gmail.com>
2021-10-04 20:07:11 +0700

    [PW1J-33] login feature

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -7,12 +7,12 @@ const nodemailer = require("../helper/nodemailer");
 module.exports = {
   // REGISTER //
   register: (req, res) => {
+    //hash pass
     let { fullName, username, email, password } = req.body;
-
-    password = Crypto.createHmac("sha1", "hash123") //hash pass
+    password = Crypto.createHmac("sha1", "hash123")
       .update(password)
       .digest("hex");
-
+    //query
     let registerQuery = `INSERT INTO users VALUES (
           null,
           ${db.escape(fullName)},
@@ -32,7 +32,6 @@ module.exports = {
         console.log(err);
         res.status(500).send(err);
       }
-
       //If Inject Data Success//
       if (results) {
         //get user data
@@ -42,7 +41,6 @@ module.exports = {
             console.log(err);
             res.status(500).send(err);
           }
-
           //Create Token
           let { idUser, fullName, username, email, idRole, isVerified } =
             results1[0];
@@ -54,7 +52,6 @@ module.exports = {
             idRole,
             isVerified,
           });
-
           //Send Verify Email
           let mail = {
             from: `Admin <ayyasluthfi@gmail.com>`,
@@ -91,6 +88,59 @@ module.exports = {
         res.status(500).send({ messege: "acount unverified", success: false });
       }
       res.status(200).send({ messege: "acount verified", success: true });
+    });
+  },
+
+  // LOGIN //
+  login: (req, res) => {
+    //hash password
+    req.body.password = Crypto.createHmac("sha1", "hash123")
+      .update(req.body.password)
+      .digest("hex");
+    //query
+    let loginQuery = `SELECT * FROM users WHERE email=${db.escape(
+      req.body.email
+    )} AND password =${db.escape(req.body.password)} ;`;
+    //inject query
+    db.query(loginQuery, (err, results) => {
+      //if error
+      if (err) res.status(500).send(err);
+      //if not error create token n check status
+      if (results[0]) {
+        //user data
+        let {
+          idUser,
+          fullName,
+          username,
+          email,
+          password,
+          idRole,
+          isVerified,
+        } = results[0];
+        //check verified
+        if (isVerified != 1) {
+          res
+            .status(200)
+            .send({ messege: "account not verified", success: false });
+        } else {
+          //Create Token
+          let Token = createToken({
+            idUser,
+            fullName,
+            username,
+            email,
+            password,
+            idRole,
+            isVerified,
+          });
+          res.status(200).send({
+            messege: "Login Success",
+            success: true,
+            Token,
+            dataLogin: results[0],
+          });
+        }
+      }
     });
   },
 };
--- be/routers/userRouter.js
+++ be/routers/userRouter.js
@@ -7,5 +7,6 @@ const router = express.Router();
 //ROUTER METHOD
 router.post("/register", userController.register);
 router.patch("/verification", authToken, userController.verification);
+router.post("/login", userController.login);
 
 module.exports = router;
