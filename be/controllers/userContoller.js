6dab3c73dc1d41517c6d4f6766cd39f7f7f83ebc
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2021-10-05 09:25:21 +0700

    [PW1J-41] Backend patch data pribadi user, berhasil tes postman

--- be/controllers/userContoller.js
+++ be/controllers/userContoller.js
@@ -2,8 +2,12 @@ const { db } = require('../database/index')
 
 
 module.exports = {
-    get: (req, res) => {
-        let scriptQuery = `SELECT * FROM users`
+    editData: (req, res) => {
+        let { id, fullName, username, email, gender, dateOfBirth } = req.body
+
+        // res.status(200).send(req.body)
+        let scriptQuery = `UPDATE users SET fullName=${db.escape(fullName)}, username=${db.escape(username)},email=${db.escape(email)},gender=${db.escape(gender)},dateOfBirth=${db.escape(dateOfBirth)} WHERE idUser=${db.escape(id)}`
+        console.log(scriptQuery);
         db.query(scriptQuery, (err, results) => {
             // err = true
             if (err) res.status(500).send({ message: "Gagal mengambil data di database", success: false, err })
--- be/routers/userRouter.js
+++ be/routers/userRouter.js
@@ -2,6 +2,6 @@ const express = require('express')
 const { userController } = require('../controllers/index')
 const routers = express.Router()
 
-routers.get('/get', userController.get)
+routers.patch('/setting', userController.editData)
 
 module.exports = routers
