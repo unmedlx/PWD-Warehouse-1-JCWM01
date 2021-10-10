dc95bdc30f2a1e3676cc46916e5771dab29266b0
lianeddy <49635006+lianeddy@users.noreply.github.com>
2021-10-10 13:20:11 +0700

    Revert "Pw1 j 34 change password"

--- be/routers/index.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require('./userRouter')
-const uploaderRouter = require('./uploaderRouter')
-const addressRouter = require('./adressRouter')
-
-module.exports = {
-  userRouter,
-  uploaderRouter,
-  addressRouter
-}
--- fe/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import usersReducer from "./users";
-import addressesReducer from "./addresses";
-
-export default combineReducers({
-  users: usersReducer,
-  addresses: addressesReducer
-});
